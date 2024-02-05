6343727896af95c53e758ec3530980139032f3e2
Triskel26 <redstoneur26@gmail.com>
2024-02-05 15:50:36 +0100

    backup

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 
-
-document.addEventListener('DOMContentLoaded', function() {​​ alert('La page est complètement chargée !');
-}​);
-
 document.getElementById('super_secret_button').addEventListener('click', function() {
     window.location = 'https://www.youtube.com/watch?v=xvFZjo5PgG0&ab_channel=Duran';
 });
