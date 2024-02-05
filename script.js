663ad1bbcd75ab36ee6c0d4c2f790a40588a212f
Triskel26 <redstoneur26@gmail.com>
2024-02-05 15:40:06 +0100

    commit 3

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
 
+document.addEventListener('DOMContentLoaded', function() {​​ alert('La page est complètement chargée !');
+}​);
+
 document.getElementById('super_secret_button').addEventListener('click', function() {
     window.location = 'https://www.youtube.com/watch?v=xvFZjo5PgG0&ab_channel=Duran';
 });
